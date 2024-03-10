88343c7e564cf0bfbc821bbab04a6f6bf56d623a
Vincent Boettcher <vin.br@runbox.no>
2024-03-10 00:54:14 +0000

    feat: UI update and back-end fixes

--- trainer.py
+++ trainer.py
@@ -2,5 +2,5 @@ from imageai.Classification.Custom import ClassificationModelTrainer
 
 model_trainer = ClassificationModelTrainer()
 model_trainer.setModelTypeAsDenseNet121()
-model_trainer.setDataDirectory("./.data/brain_dataset/training")
+model_trainer.setDataDirectory("./data/brain_dataset/training")
 model_trainer.trainModel(num_experiments=100, batch_size=32)
